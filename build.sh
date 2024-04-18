1aa53acd0748263620bf212a69575fdabbd9881d
pkgdemon <pkgdemonteam@gmail.com>
2024-04-18 16:50:43 -0400

    Fix workspace settings in script

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ CPUS=$(nproc)
 export GNUSTEP_INSTALLATION_DOMAIN=LOCAL
 . /Developer/Makefiles/GNUstep.sh
 
-if [ -f "/__w/system/system/root_amd64.zip" ]; then
-  export SRC="/__w/system/system/"
+if [ -f "/__w/applications/applications/system_amd64.zip" ]; then
+  export SRC="/__w/applications/applications/"
 fi
 
-if [ -f "/home/runner/work/system/system/root_arm64.zip" ]; then
-  export SRC="/home/runner/work/system/system/"  
+if [ -f "/home/runner/work/applications/applications/system_arm64.zip" ]; then
+  export SRC="/home/runner/work/applications/applications/"  
 fi
 
 cd ${SRC}/gap/ported-apps/Games/Chess && gmake -j"${CPUS}" || exit 1 || exit 1 && gmake install
